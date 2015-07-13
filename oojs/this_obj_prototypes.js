bf15526a210837e891255f8d7c971414e196383a
santana kenner <sskenner@gmail.com>
2015-07-13 08:49:48 +0000

    update ydkjs notes

--- oojs/this_obj_prototypes.js
+++ oojs/this_obj_prototypes.js
@@ -885,7 +885,7 @@ class ABC inherits Task {
 
 // ..Delegation Theory
 // behavior delegation instead of classes
-Task = {
+var Task = {
   setID:  function(ID) {
             this.id = ID;
           },
@@ -895,7 +895,7 @@ Task = {
 };
 
 // make 'XYZ' delegate to 'Task'
-XYZ = Object.create( Task );
+var XYZ = Object.create( Task );
 
 XYZ.prepareTask = function(ID,Label)  {
                     this.setID( ID );
@@ -910,3 +910,501 @@ XYZ.outputTaskDetails = function() {
 // ABC = Object.create( Task );
 // ABC ... = ...
 // OLOO - Objects Linked to Other Objects
+///
+// Behavior delegation - to let some object (XYZ) provide a delegation (to Task) for property or method references if they are not found on the object (XYZ)
+
+// ...Mututal delegation
+///
+// ...Mutual delegation (disallowed)
+///
+// ...Debugged
+// "class constructor" style JS code.. Chrome Dev Tools console:
+function Foo() {}
+
+var a1 = new Foo();
+
+a1; // Foo {} [tracks internal property] >> Object {} [firefox DTs]
+
+///
+function Foo() {}
+
+var a1 = new Foo();
+
+a1.constructor; // Foo(){}
+a1.constructor.name; // "Foo"
+
+///
+function Foo() {}
+
+var a1 = new Foo();
+
+Foo.prototype.constructor = function Gotcha(){};
+
+a1.constructor;
+a1.constructor.name;
+
+///
+function Foo() {}
+
+var a1 = Object.create( Foo );
+
+a1; // Object {}
+
+Object.defineProperty( Foo, "constructor", {
+  enumerable: false,
+  value: function Gothca() {}
+});
+
+a1; // Gotcha
+
+// ..Mental Models Compared
+// classical ("protoypal") OO style:
+function Foo(who) {
+  this.me = who;
+}
+Foo.prototype.identify = function() {
+  return "i am " + this.me;
+};
+
+function Bar(who) {
+  Foo.call( this, who );
+}
+Bar.prototype = Object.create( Foo.prototype );
+
+Bar.prototype.speak = function() {
+  alert( "hello, " + this.identify() + ".");
+};
+
+var b1 = new Bar( "b1" );
+var b2 = new Bar( "b2" );
+
+b1.speak();
+b2.speak();
+
+// ..same functionality using OLOO-style:
+var Foo = {
+  init: function(who) {
+    this.me = who;
+  },
+  identify: function() {
+    return "i am " + this.me;
+  }
+};
+
+var Bar = Object.create( Foo );
+
+Bar.speak = function() {
+  console.log( "hello, " + this.identify() + ".");
+};
+
+var b1 = Object.create( Bar );
+b1.init( "b1" );
+var b2 = Object.create( Bar );
+b2.init( "b2" );
+
+b1.speak();
+b2.speak();
+
+// ..Classes vs Objects
+// ...Widget "Classes"
+// parent class
+function Widget(width,height) {
+  this.width = width || 50;
+  this.height = height || 50;
+  this.$elem = null;
+}
+
+Widget.prototype.render = function($where) {
+  if (this.$elem) {
+    this.$elem.css( {
+      width: this.width + "px",
+      height: this.height + "px"
+    }).appendTo( $where );
+  }
+};
+
+// child class
+function Button(width,height,label) {
+  // "super" constructor call
+  Widget.call( this, width, height );
+  this.label = label || "default";
+  
+  this.$elem = $( "<button>" ).text( this.label );
+}
+
+// make "Button" "inherit" from 'Widget'
+Button.prototype = Object.create( Widget.prototype );
+
+// override base "inherited" 'render(..)'
+Button.prototype.render = function($where) {
+  // "super" call
+  Widget.prototype.render.call( this, $where );
+  this.$elem.click( this.onClick.bind( this ) );
+};
+
+Button.prototype.onClick = function(evt) {
+  console.log( "Button '" + this.label + "' clicked!" );
+};
+
+$( document ).ready( function() {
+  var $body = $( document.body );
+  var btn1 = new Button( 125, 30, "Hello" );
+  var btn2 = new Button( 150, 40, "World" );
+  
+  btn1.render( $body );
+  btn2.render( $body );
+});
+
+// ...ES6 class sugar
+class Widget {
+  constructor(width,height) {
+    this.width = width || 50;
+    this.height = height || 50;
+    this.$elem = null;
+  }
+  render($where) {
+    if (this.$elem) {
+      this.$elem.css( {
+        width: this.width + "px",
+        height: this.height + "px"
+      }).appendTo( $where );
+    }
+  }
+}
+
+class Button extends Widget {
+  constructor(width,height,label) {
+    super( width, height );
+    this.label = label || "default";
+    this.$elem = $( "<button>" ).text( this.label );
+  }
+  render($where) {
+    super( $where );
+    this.$elem.click( this.onClick.bind( this ) );
+  }
+  onClick(evt) {
+    console.log( "Button '" + this.label + "' clicked!" ); 
+  }
+}
+
+$( document ).ready( function() {
+  var $body = $( document.body );
+  var btn1 = new Button( 125, 30, "Hello" );
+  var btn2 = new Button( 150, 40, "World" );
+  
+  btn1.render( $body );
+  btn2.render( $body );
+});
+
+// ..Delegating Widget Objects
+var Widget = {
+  init: function(width,height) {
+    this.width = width || 50;
+    this.height = height || 50;
+    this.$elem = null;
+  },
+  insert: function($where) {
+    if (this.$elem) {
+      this.$elem.css( {
+        width: this.width + "px",
+        height: this.height + "px"
+      }).appendTo( $where );
+    }
+  }
+};
+
+var Button = Object.create( Widget );
+
+Button.setup = function(width,height,label) {
+  // delegated call
+  this.init( width, height );
+  this.label = label || "default";
+  
+  this.$elem = $( "<button>" ).text( this.label );
+};
+Button.build = function($where) {
+  // delegated call
+  this.insert( $where );
+  this.$elem.click( this.onClick.bind( this ) );
+};
+Button.onClick = function(evt) {
+  console.log( "Button '" + this.label + "' clicked!" );
+};
+
+$( document ).ready( function() {
+  var $body = $( document.body );
+  
+  var btn1 = Object.create( Button );
+  btn1.setup( 125, 30, "Hello" );
+  
+  var btn2 = Object.create( Button );
+  btn2.setup( 150, 40, "World" );
+  
+  btn1.build( $body );
+  btn2.build( $body );
+});
+
+// ..Simpler Design
+// examination of 2 controller objects
+// Parent class
+function Controller() {
+  this.errors = [];
+}
+Controller.prototype.showDialog(title,msg) {
+  // display title & message to user in dialog
+};
+Controller.prototype.success = function(msg) {
+  this.showDialog( "Success", msg );
+};
+Controller.prototype.failure = function(err)  {
+  this.errors.push( err );
+  this.showDialog( "Error", err );
+};
+// Child class
+function LoginController() {
+  Controller.call( this );
+}
+// Link child class to parent
+LoginController.prototype = Object.create( Controller.prototype );
+LoginController.prototype.getUser = function() {
+  return document.getElementById( "login_username" ).value;
+};
+LoginController.prototype.getUser = function() {
+  return document.getElementById( "login_password" ).value;
+};
+LoginController.prototype.validateEntry = function(user,pw) {
+  user = user || this.getUser();
+  pw = pw || this.getPassword();
+  
+  if (!(user && pw)) {
+    return this.failure(
+      "please enter a username & password!"
+    );
+  }
+  else if (user.length < 5) {
+    return this.failure(
+      "password must be 5+ characters!"
+    );
+  }
+  
+  // got here? validated!
+  return true;
+};
+// override to extend base 'failure()'
+LoginController.prototype.failure = function(err) {
+  // "super" call
+  Controller.prototype.failure.call(
+    this,
+    "login invalid: " + err
+  );
+};
+
+// Child class
+function AuthController(login) {
+  Controller.call( this );
+  // in addition to inheritance, we also need composition
+  this.login = login;
+}
+// Link child class to parent
+AuthController.prototype = Object.create( Controller.prototype );
+AuthController.prototype.server = function(url,data) {
+  return $.ajax( {
+    url: url,
+    data: data
+  });
+};
+AuthController.prototype.checkAuth = function() {
+  var user = this.login.getUser();
+  var pw = this.login.getPassword();
+  
+  if (this.login.validateEntry( user, pw )) {
+    this.server( "/check-auth",{
+      user: user,
+      pw: pw
+    })
+    .then( this.success.bind( this ))
+    .fail( this.failure.bind( this ));
+  }
+};
+// Override to extend base 'success()'
+AuthController.prototype.success = function() {
+  // "super" call
+  Controller.prototype.success.call( this, "Authenticated!" );
+};
+// Override to extend base 'failure()'
+AuthController.prototype.failure = function(err) {
+  // "super" call
+  Controller.prototype.failure.call(
+    this,
+    "Auth Failed: " + err
+  );
+};
+var auth = new AuthController();
+auth.checkAuth(
+  // in addition to inheritance, we also need composition
+  new LoginController()
+);
+
+// ..De-class-ified
+// OLOO-style behavior
+var LoginController = {
+  errors: [],
+  getUser: function() {
+    return document.getElementById(
+      "login_username"
+    ).value;
+  },
+  validateEntry: function(user,pw) {
+    user = user || this.getUser();
+    pw = pw || this.getPassword();
+    
+    if (!(user && pw)) {
+      return this.failure(
+        "please enter a username & password!"
+      );
+    }
+    else if (user.length < 5) {
+      return this.failure(
+        "password must be 5+ characters!"
+      );
+    }
+    
+    // got here? validated!
+    return true;
+  },
+  showDialog: function(title,msg) {
+    // display success message to user in dialog
+  },
+  failure: function(err) {
+    this.errors.push( err );
+    this.showDialog( "Error", "Login invalid: " + err );
+  }
+};
+
+// Link 'AuthController' to delegate to 'LoginController'
+var AuthController = Object.create( LoginController );
+
+AuthController.errors = [];
+AuthController.checkAuth = function() {
+  var user = this.getUser();
+  var pw = this.getPassword();
+  
+  if (this.validateEntry( user, pw )) {
+    this.server( "/check-auth",{
+      user: user,
+      pw: pw
+    })
+    .then( this.accepted.bind( this ) )
+    .fail( this.rejected.bind( this ));
+  }
+};
+AuthController.server = function(url,data) {
+  return $.ajax( {
+    url: url,
+    data: data
+  });
+};
+AuthController.accepted = function() {
+  this.showDialog( "Success", "Authenticated!" )
+};
+AuthController.rejected = function(err) {
+  this.failure( "Auth Failed: " + err );
+};
+
+// no need to instantiate to perform task
+AuthController.checkAuth();
+
+// if need to create one or more additional objects in the delegation chain
+var controller1 = Object.create( AuthController );
+var controller2 = Object.create( AuthController );
+
+// ..Nicer Syntax
+class Foo {
+  methodName() {
+    /*..*/
+  }
+}
+
+// using concise method declarations
+var LoginController = {
+  error: [],
+  getUser() {
+    // look ma, no 'function'!
+  },
+  getPassword() {
+    // ..
+  }
+  // ..
+};
+
+// use nicer object literal syntax w/ concise methods!
+var AuthController = {
+  errors: [],
+  checkAuth() {
+    // ..
+  },
+  server(url,data) {
+    // ..
+  }
+  // ..
+};
+// NOW, link 'AuthController' to delegate to 'LoginController'
+Object.setPrototypeOf( AuthController, LoginController );
+
+// ..Unlexical
+var Foo = {
+  bar() { /* .. */ },
+  baz: function baz() { /* .. */ }
+};
+// syntax de-sugaring on how it will work
+var Foo = {
+  bar: function() { /* .. */ },
+  baz: function baz() { /* .. */ }
+};
+
+///
+var Foo = {
+  bar: function(x) {
+    if (x < 10) {
+      return Foo.bar( x * 2 );
+    }
+    return x;
+  },
+  baz: function baz(x) {
+    if (x < 10) {
+      return baz( x * 2 );
+    }
+    return x;
+  }
+};
+
+// ..Introspection
+function Foo() {
+  // ..
+}
+Foo.prototype.something = function() {
+  // ..
+}
+
+var a1 = new Foo();
+
+// later
+
+if (a1 instanceof Foo) {
+  a1.something();
+}
+
+///
+function Foo()  {/* .. */ }
+Foo.prototype...
+
+function Bar()  { /* .. */ }
+Bar.prototype = Object.create( Foo.prototype );
+
+var b1 = new Bar( "b1" );
+// various checks might need to perform for the above
+
+
+
+
+
